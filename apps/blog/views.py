e9f92e101c08b1ffca5f709614f88bf709cc1ec3
Андрей Седельников <nikolay.kalitkin@gmail.com>
2025-01-21 02:45:00 +0600

    добавлена пагинация

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,6 +6,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,9 +26,11 @@ class PostDetailView(DetailView):
         
 
 class PostFromCategory(ListView):
+    model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     category = None
+    paginate_by = 1
 
     def get_queryset(self):
         self.category = Category.objects.get(slug=self.kwargs['slug'])
