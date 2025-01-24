609019000c122f2747d8a6c20372c2fe82a8bbd8
Андрей Седельников <nikolay.kalitkin@gmail.com>
2025-01-24 23:49:42 +0600

    отобразим форму, в которой пользователи смогут обновлять свой профиль. Создание представлений для просмотра профиля, редактирования профиля

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, UpdateView
+from django.db import transaction
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Profile
+from .forms import UserUpdateForm, ProfileUpdateForm
+
+
+class ProfileDetailView(DetailView):
+    """
+    Представление для просмотра профиля
+    """
+    model = Profile
+    context_object_name = 'profile'
+    template_name = 'accounts/profile_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Профиль пользователя: {self.object.user.username}'
+        return context
+
+
+class ProfileUpdateView(UpdateView):
+    """
+    Представление для редактирования профиля
+    """
+    model = Profile
+    form_class = ProfileUpdateForm
+    template_name = 'accounts/profile_edit.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Редактирование профиля пользователя: {self.request.user.username}'
+        if self.request.POST:
+            context['user_form'] = UserUpdateForm(self.request.POST, instance=self.request.user)
+        else:
+            context['user_form'] = UserUpdateForm(instance=self.request.user)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        user_form = context['user_form']
+        # Это контекстный менеджер, который управляет транзакциями в БД. Так как нам необходимо сохранять две формы, нам нужно быть уверенными что обе формы будут успешно сохранены. Вот он и группирует ряд операций с БД в одну транзакцию, что любые изменения в базе данных будут зафиксированы при выполнении его блока без исключений.
+        with transaction.atomic():
+            if all([form.is_valid(), user_form.is_valid()]):
+                user_form.save()
+                form.save()
+            else:
+                context.update({'user_form': user_form})
+                return self.render_to_response(context)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('profile_detail', kwargs={'slug': self.object.slug})
